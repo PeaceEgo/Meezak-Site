60d83c1e722598b4dc77de7132e4806de223ad4c
Peace Nnorom <nnorompeace14@gmail.com>
2025-04-20 23:09:12 +0100

    added testimonial and workspace section

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import TestimonialSlider from "@/components/testimonial"
 import FAQAccordion from "../components/faqAccordion"
 import ServicesSection from "../components/servicesSect"
 import WhyChooseUs from "../components/whyChoose"
 import WorkspaceGallery from "../components/workSpace"
 
-=======
 import Header from '../components/header';
 import Hero from '../components/hero';
 import TrustedLogos from '../components/TrustedLogos';
@@ -13,28 +11,21 @@ import FAQAccordion from "../components/faqAccordion"
 import ServicesSection from "../components/servicesSect"
 import WhyChooseUs from "../components/whyChoose"
 import GetStarted from "../components/GetStarted"
->>>>>>> a427ad2bd496051435732341f6d5da9879857856
 
 
 function HomePage() {
   return (
-<<<<<<< HEAD
-    <div className="w-[100vw] overflow-hidden"> 
-          <ServicesSection/>
-          <WhyChooseUs/>
-          <FAQAccordion/>
-      <WorkspaceGallery/>
-      <TestimonialSlider/>
-=======
-    <div className="w-full h-[100%] bg-[ #EEEEEE]"> 
+   
+      <div className=" w-[100vw] overflow-hidden"> 
           <Header/>
           <Hero/>
           <TrustedLogos/>
           <ServicesSection/>
           <WhyChooseUs/>
           <FAQAccordion/>
+        <WorkspaceGallery />
+        <TestimonialSlider />
           <GetStarted/>
->>>>>>> a427ad2bd496051435732341f6d5da9879857856
     </div>
   )
 }
